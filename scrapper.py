cd3504c3dd8a90e6dcfc7d17cbe94de3f99e05d8
DevelopSoo <dhnp6803@naver.com>
2020-12-20 13:29:55 +0900

    Make structures of project

---
+++ scrapper.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+"""
+로켓펀치 채용 주소 : https://www.rocketpunch.com/jobs
+로켓펀치 웹개발 및 파이썬 : https://www.rocketpunch.com/jobs?job=1&specialty=Python
+원티드 파이썬 개발자 : https://www.wanted.co.kr/wdlist/518/899?country=kr&job_sort=job.latest_order&years=-1&locations=all
+프로그래머스 파이썬 개발자 백엔드 개발자: https://programmers.co.kr/job?job_position%5Btags%5D%5B%5D=Python&amp;job_position%5Bjob_category_ids%5D%5B%5D=1&amp;job_position%5Bdummy%5D=0&amp;_=1608438200977
+"""
+
